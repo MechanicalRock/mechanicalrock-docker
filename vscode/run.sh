64cb6e01243989aa0a8ee4721ec5ba62bdd4b997
temyers <tim.myerscough@mechanicalrock.io>
2018-09-02 09:53:00 +0800

    start vscode on background thread for interactive use

--- vscode/run.sh
+++ vscode/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 BASEDIR="$( cd "$(dirname "$BASH_SOURCE")/.." ; pwd -P )"
-DOCKER_COMPOSE="docker-compose -f $BASEDIR/docker-compose.yml up "
+DOCKER_COMPOSE="docker-compose -f $BASEDIR/docker-compose.yml run --rm "
 
 code="$DOCKER_COMPOSE vscode"
 
@@ -12,7 +12,7 @@ path="${1/#\~/$HOME}"
 if [[ -d $path ]]
 then
     path=$(realpath $path)
-    $DOCKER_COMPOSE -v "$path:/home/dev/workspace" vscode .
+    $DOCKER_COMPOSE -v "$path:/home/dev/workspace" vscode . &
 
 elif [[ -f $path ]]
 then
@@ -22,7 +22,7 @@ then
     parent_dir="$( cd "$(dirname "$path")" ; pwd -P )"
     # cd $parent_dir
     # pwd
-    $DOCKER_COMPOSE -v "$parent_dir:/home/dev/workspace" vscode  $(basename $(realpath $path))
+    $DOCKER_COMPOSE -v "$parent_dir:/home/dev/workspace" vscode  $(basename $(realpath $path)) &
 else
-    $code
+    $code &
 fi
