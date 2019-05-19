2f3e42d38fc215e203b724c1aff5298adfaaba65
temyers <tim.myerscough@mechanicalrock.io>
2019-05-19 13:01:01 +0800

    Add npx.  Limited support for OSX - realpath not available.  No guarantees on X11 support (external dependency)

--- setenv.sh
+++ setenv.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
 
-SCRIPT_DIR=$(realpath $0)
-BASEDIR="$( cd "$(dirname "$SCRIPT_DIR")" ; pwd -P )"
+osx='Darwin'
+if [[ uname -eq $osx ]]
+then
+	BASEDIR="$( cd "$(dirname "$1")" ; pwd -P )"
+else
+	SCRIPT_DIR=$(realpath $0)
+	BASEDIR="$( cd "$(dirname "$SCRIPT_DIR")" ; pwd -P )"
+fi
 
 DOCKER_COMPOSE="docker-compose -f $BASEDIR/docker-compose.yml run --rm "
 
 alias aws="$DOCKER_COMPOSE aws"
 alias sam="$DOCKER_COMPOSE sam"
 alias npm="$DOCKER_COMPOSE npm"
+alias npx="$DOCKER_COMPOSE npx"
 alias cfn-lint="$DOCKER_COMPOSE cfn-lint"
 alias psql="$DOCKER_COMPOSE psql"
 alias mosh="$DOCKER_COMPOSE mosh"
