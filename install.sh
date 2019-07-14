7f7c336769d90c6fe939d7be446567066ab0c66d
temyers <tim.myerscough@mechanicalrock.io>
2019-07-14 08:43:42 +0800

    fix install script - pass arguments to container invocation

--- install.sh
+++ install.sh
@@ -12,40 +12,40 @@ BASEDIR="$( cd "$(dirname "$SCRIPT_DIR")" ; pwd -P )"
 DOCKER_COMPOSE="docker-compose -f $BASEDIR/docker-compose.yml run --rm "
 
 cat <<EOM >$HOME/bin/aws
-	$DOCKER_COMPOSE aws
+	$DOCKER_COMPOSE aws \$@
 EOM
 cat <<EOM >$HOME/bin/sam
-	$DOCKER_COMPOSE sam
+	$DOCKER_COMPOSE sam \$@
 EOM
 cat <<EOM >$HOME/bin/npm
-	$DOCKER_COMPOSE npm
+	$DOCKER_COMPOSE npm \$@
 EOM
 cat <<EOM >$HOME/bin/npx
-	$DOCKER_COMPOSE npx
+	$DOCKER_COMPOSE npx \$@
 EOM
 cat <<EOM >$HOME/bin/cfn-lint
-	$DOCKER_COMPOSE cfn-lint
+	$DOCKER_COMPOSE cfn-lint \$@
 EOM
 cat <<EOM >$HOME/bin/psql
-	$DOCKER_COMPOSE psql
+	$DOCKER_COMPOSE psql \$@
 EOM
 cat <<EOM >$HOME/bin/mosh
-	$DOCKER_COMPOSE mosh
+	$DOCKER_COMPOSE mosh \$@
 EOM
 cat <<EOM >$HOME/bin/cfn-nag
-	$DOCKER_COMPOSE cfn-nag
+	$DOCKER_COMPOSE cfn-nag \$@
 EOM
 cat <<EOM >$HOME/bin/code
-	$BASEDIR/vscode/run.sh
+	$BASEDIR/vscode/run.sh \$@
 EOM
 cat <<EOM >$HOME/bin/slack
-	$DOCKER_COMPOSE slack
+	$DOCKER_COMPOSE slack \$@
 EOM
 cat <<EOM >$HOME/bin/gimp
-	$DOCKER_COMPOSE gimp
+	$DOCKER_COMPOSE gimp \$@
 EOM
 cat <<EOM >$HOME/bin/chrome
-	$DOCKER_COMPOSE chrome
+	$DOCKER_COMPOSE chrome \$@
 EOM
 
 ## THIS LINE SHOULD BE LAST
