3e48650e3448fa4f45b32459b15bf03a724d1b22
temyers <tim.myerscough@mechanicalrock.io>
2019-10-24 13:51:06 +0800

    Fix crash of VSCode on 4k display.  Tag containers during re-build.

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-docker-compose build --no-cache $@
+for ARG in "$@"
+do
+  docker tag "mechanicalrockdocker_$ARG:latest" "mechanicalrockdocker_$ARG:prev"
+  docker-compose build --no-cache $ARG
+done
+
