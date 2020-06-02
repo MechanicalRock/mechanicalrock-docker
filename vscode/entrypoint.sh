15efe3aa9faeb74b4945930c41eab0fb399c00da
temyers <tim.myerscough@mechanicalrock.io>
2020-06-02 11:18:30 +0800

    upgrade aws-cli. Use python3.  Upgrade VSCode

--- vscode/entrypoint.sh
+++ vscode/entrypoint.sh
@@ -3,6 +3,9 @@
 # Start DBus session - required to remove errors
 dbus-daemon --config-file=/usr/share/dbus-1/session.conf &
 
+# Set TTY got GPG - git commit signing.
+export GPG_TTY=$(tty)
+
 # FIXME - --user-data-dir only allows one running instance...
 
 # code --verbose --user-data-dir /vscode-data
