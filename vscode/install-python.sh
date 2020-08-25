81e5aa75c7de1fdaac32984b2e650ff87bac5555
temyers <tim.myerscough@mechanicalrock.io>
2020-08-25 15:23:36 +0800

    upgrade to python 3.7

--- vscode/install-python.sh
+++ vscode/install-python.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-apt-get install -y python3-pip python3-dev
+# https://linuxize.com/post/how-to-install-python-3-7-on-ubuntu-18-04/
+apt install -y software-properties-common
+
+add-apt-repository -y ppa:deadsnakes/ppa
+
+#apt-get install -y python3-pip python3-dev
+apt-get install -y python3.7 python3.7-dev 
 
 cd /usr/local/bin
-ln -s /usr/bin/python3 python
+ln -s /usr/bin/python3.7 python
 pip3 install --upgrade pip
 
